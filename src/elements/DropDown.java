4e8d9fead780095ec7a8b277d76be4d61ac58eb5
Vedank99 <51454248+Vedank99@users.noreply.github.com>
2020-12-08 22:24:19 +0530

    Unnecessary things removed

--- src/elements/DropDown.java
+++ src/elements/DropDown.java
@@ -1,22 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package elements;
 
-import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.util.Scanner;
 import javax.swing.JComboBox;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-/**
- *
- * @author admin
- */
 public class DropDown extends BasicElement{
     
     public int numOfOptions;
